1831493c19379c5d63ee44c8f3e98a3fc9ffaee5
Polsander <oerdmann@uwaterloo.ca>
2022-08-12 19:26:44 -0400

    front-end setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import Main from './pages/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="OliverErdmann">
+        <Main/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//below are imports for any components that may be used
+
+function Main() {
+  return (
+    <div>
+        <p>Woah step bro! What are you doing!?</p>
+    </div>
+  )
+}
+
+export default Main
