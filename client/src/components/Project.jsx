af38c391347305e3bdb3f5f94b83f44f71c6f120
Polsander <oerdmann@uwaterloo.ca>
2022-08-20 01:21:14 -0700

    progress on projects section

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,35 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import Row from 'react-bootstrap/esm/Row'
 import Col from 'react-bootstrap/esm/Col'
 
 import Button from 'react-bootstrap/esm/Button'
 
+import styles from './Project.module.css'
+
 function Project(props) {
-    if (props.side === "right") {
+    // Need to use a hook to check for mobile screen
+    const [width, setWidth] = useState(window.screen.width);
+    const handleScreenSize = () => {
+        setWidth(window.screen.width);
+    }
+
+    useEffect(()=>{
+        window.addEventListener('resize',handleScreenSize);
+        return ()=> {
+            window.removeEventListener('resize',handleScreenSize);
+        }
+    }, []);
+    const isMobile = width <=768
+
+    const img_class = `${styles.img}`
+
+    // Rendering JSX code starts here
+    if (props.side === "right" && !isMobile) {
         return (
-            <Container>
+            <Container className='mb-3'>
                 <Row>
                     <Col>
-                        <p>{props.description}</p>
-                        <Button>{props.buttonTitle}</Button>
+                        <h3><strong>{props.projectTitle}</strong></h3>
+                        <p className='1h-base fs-4 mt-3 cstm-text'>{props.description}</p>
+                        <Button className={styles.btn}>{props.buttonTitle}</Button>
                     </Col>
                     <Col>
-                        <img src={props.imgSrc} alt="project preview/emblem" />
+                        <img className={img_class} src={props.imgSrc} alt="project preview/emblem" />
                     </Col>
                 </Row>
             </Container>
         )
     } else {
         return (
-            <Container>
+            <Container className='mb-3'>
                 <Row>
                     <Col>
-                        <img src={props.imgSrc} alt="project preview/emblem" />
+                        <img className={img_class} src={props.imgSrc} alt="project preview/emblem" />
                     </Col>
                     <Col>
-                        <p>{props.description}</p>
-                        <Button>{props.buttonTitle}</Button>
+                        <h3>{props.projectTitle}</h3>
+                        <p className='1h-base fs-4 mt-3 cstm-text'>{props.description}</p>
+                        <Button className={styles.btn}>{props.buttonTitle}</Button>
                     </Col>
                 </Row>
             </Container>
