0afb9a0ddd2918ef8490662cda3e53942021db6b
Polsander <oerdmann@uwaterloo.ca>
2022-08-23 21:20:05 -0700

    trying this

--- index.js
+++ index.js
@@ -8,13 +8,20 @@ const app = express();
 
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // // Routes we can use
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 //   });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
+  app.get('*', (req,res)=>{
+    req.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+  })
+}
 
 app.listen(PORT,() => {
 console.log(`Server is listening on ${PORT}`)
