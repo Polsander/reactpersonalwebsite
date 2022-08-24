bf00d8610081d287d0b102a67fe7f4ccccc974f0
Polsander <oerdmann@uwaterloo.ca>
2022-08-23 17:38:25 -0700

    fix overflow problems

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -29,14 +29,14 @@ function Project(props) {
         return (
             <Container className='mb-3'>
                 <Row>
-                    <Col xs={12} md={6}>
+                    <Col sm={12} md={6}>
                         <h3><strong>{props.projectTitle}</strong></h3>
                         <p className='1h-base fs-4 mt-3 cstm-text'>{props.description}</p>
                         <a href={props.buttonLink}>
                             <Button className={styles.btn}>{props.buttonTitle}</Button>
                         </a>
                     </Col>
-                    <Col xs={12} md={6}>
+                    <Col sm={12} md={6}>
                         <img className={img_class} src={props.imgSrc} alt="project preview/emblem" />
                     </Col>
                 </Row>
@@ -46,10 +46,10 @@ function Project(props) {
         return (
             <Container className='mb-3'>
                 <Row>
-                    <Col xs={12} md={6} className='d-flex justify-content-center'>
+                    <Col sm={12} md={6} className='d-flex justify-content-center'>
                         <img className={img_class} src={props.imgSrc} alt="project preview/emblem" />
                     </Col>
-                    <Col xs={12} md={6}>
+                    <Col sm={12} md={6}>
                         <h3>{props.projectTitle}</h3>
                         <p className='1h-base fs-4 mt-3 cstm-text'>{props.description}</p>
                         <a href={props.buttonLink}>
