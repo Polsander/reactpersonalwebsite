18cec8f8a1cc63090655b2b68cb6b02f5835c13a
Polsander <oerdmann@uwaterloo.ca>
2022-08-23 19:56:33 -0700

    move server to root

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const app = express();
 
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-// Routes we can use
+// // Routes we can use
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+//   });
 
 app.listen(PORT,() => {
 console.log(`Server is listening on ${PORT}`)
