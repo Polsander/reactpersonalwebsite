5b6ee91fba6ec1a8824c0d7f033f856dafb3356b
Polsander <oerdmann@uwaterloo.ca>
2022-08-23 18:25:09 -0700

    middleware to direct heroku to front-end

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const path = require("path");
+
+//This will create a middleware.
+//When you navigate to the root page, it would use the built react-app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Routes we can use
 
